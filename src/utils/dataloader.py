7f484bf360e1f10cea9767676b37d218f6cf4b65
cameroncruz <cameroncruz@gmail.com>
2020-05-22 20:47:18 -0700

    Add tokenization, tests, and comments

--- src/utils/dataloader.py
+++ src/utils/dataloader.py
@@ -1,18 +1,42 @@
 import tensorflow as tf
+import json
 from typing import Callable
 
 
-def create_parse_fn(features_path: str) -> Callable:
+def create_parse_fn(features_path: str, vocab_file: str) -> Callable:
+    """Create a function to parse dataset examples.
 
-    def parse_example(id: str, folder: str, signer: str, annotation: str) -> (tf.float32, tf.string):
-        frames, _ = tf.py_function(read_frames, [features_path, folder], (tf.float32, tf.int32))
+    Args:
+        features_path: Path to folders of frames.
+        vocab_file: JSON of saved Keras Tokenizer.
 
-        return frames, annotation
+    Returns:
+        Function that performs the following transformation:
+        - expects id, folder, signer, and annotation
+        - returns preprocessed frames and tokenized annotation converted to word indices
+    """
+    with open(vocab_file) as f:
+        tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(json.load(f))
+
+    def parse_example(id: str, folder: str, signer: str, annotation: str) -> (tf.float32, tf.int32):
+        frames, _ = tf.py_function(read_frames, [features_path, folder], [tf.float32, tf.int32])
+        tokenized_annotation = tf.py_function(create_tokenize_fn(tokenizer), [annotation], tf.int32)
+
+        return frames, tokenized_annotation
 
     return parse_example
 
 
 def read_frames(features_path: str, folder: str) -> (tf.float32, tf.int32):
+    """Read video frames from folder of frame images.
+
+    Args:
+        features_path: Path to folders of frames.
+        folder: Folder containing frames for a specific video.
+
+    Returns:
+        Frames stacked temporally as a Tensor and number of frames in the video.
+    """
     frame_files = tf.io.gfile.glob(tf.strings.join([features_path, folder]).numpy().decode('utf-8'))
     num_frames = len(frame_files)
 
@@ -21,9 +45,36 @@ def read_frames(features_path: str, folder: str) -> (tf.float32, tf.int32):
     return frames, num_frames
 
 
-def read_and_resize_img(filename: str) -> tf.float32:
+def read_and_resize_img(filename: str, preprocess='mobilenet_v2') -> tf.float32:
+    """Read an image file and resize HxW. Optionally, apply preprocessing.
+
+    Args:
+        filename: Image file.
+
+    Returns:
+        Preprocessed tf.float32 Tensor of shape (224, 224, 3) of given image.
+    """
     img = tf.io.read_file(filename)
     img = tf.io.decode_image(img)
     img = tf.image.resize(img, (224, 224))
-    img = tf.keras.applications.mobilenet_v2.preprocess_input(img)
+    if preprocess == 'mobilenet_v2':
+        img = tf.keras.applications.mobilenet_v2.preprocess_input(img)
     return img
+
+
+def create_tokenize_fn(tokenizer: tf.keras.preprocessing.text.Tokenizer, start_token: str = "<START>", end_token: str = "<END>") -> Callable:
+    """Create a function to tokenize annotations.
+
+    Args:
+        tokenizer: Keras tokenizer to use.
+        start_token: Start token used in corpus.
+        end_token: End token used in corpus.
+
+    Returns:
+        Annotation that has been tokenized and converted to a sequence of word indices.
+    """
+    def tokenize(annotation: str):
+        tokenized = tokenizer.texts_to_sequences([tf.strings.join([start_token, annotation, end_token], separator=" ").numpy().decode('utf-8')])
+        return tokenized
+
+    return tokenize
--- src/utils/tests/dataloader_test.py
+++ src/utils/tests/dataloader_test.py
@@ -1,16 +1,19 @@
 import tensorflow as tf
 import os
-from utils.dataloader import create_parse_fn, read_frames, read_and_resize_img
+import json
+from utils.dataloader import create_parse_fn, read_frames, read_and_resize_img, create_tokenize_fn
 
 
 class DataloaderTest(tf.test.TestCase):
     def setUp(self):
         self.data_path = "../debug_data/phoenix-2014-multisigner/"
         self.features_path = self.data_path + "features/fullFrame-210x260px/"
+        self.vocab_file = "../debug_data/phoenix-tokenizer.json"
         self.id = "01April_2010_Thursday_heute_default-1"
         self.folder = "01April_2010_Thursday_heute_default-1/1/*.png"
         self.signer = "Signer04"
         self.annotation = "ICH OSTERN WETTER ZUFRIEDEN MITTAG TEMPERATUR  SUED WARM MEIN NICHT"
+        self.tokenized_annotation = [2, 254, 479, 31, 641, 93, 73, 22, 82, 490, 80, 3]
         self.num_frames = 215
 
     def testReadFrames(self):
@@ -33,15 +36,24 @@ class DataloaderTest(tf.test.TestCase):
 
         self.assertEqual(img.shape, expected_shape)
 
+    def testCreateTokenizeFn(self):
+        with open(self.vocab_file) as f:
+            tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(json.load(f))
+
+        tokenize_fn = create_tokenize_fn(tokenizer)
+        tokenized = tokenize_fn(self.annotation)
+
+        self.assertEqual(tokenized[0], self.tokenized_annotation)
+
     def testParseExample(self):
         if not os.path.isdir(self.features_path):
             self.skipTest(reason="Debug data not found.")
 
-        parse_example = create_parse_fn(self.features_path)
+        parse_example = create_parse_fn(self.features_path, self.vocab_file)
         frames, label = parse_example(self.id, self.folder, self.signer, self.annotation)
 
         self.assertEqual(frames.shape, (self.num_frames, 224, 224, 3))
-        self.assertEqual(label, self.annotation)
+        self.assertAllEqual(label, self.tokenized_annotation)
 
     def testMakeDataset(self):
         csv_path = self.data_path + "annotations/manual/dev.corpus.csv"
@@ -51,4 +63,4 @@ class DataloaderTest(tf.test.TestCase):
             self.skipTest(reason="Debug data not found.")
 
         dataset = tf.data.experimental.CsvDataset(filenames=csv_path,  record_defaults=default_types, field_delim="|", header=True)
-        dataset.map(create_parse_fn(self.features_path))
+        dataset = dataset.map(create_parse_fn(self.features_path, self.vocab_file))
