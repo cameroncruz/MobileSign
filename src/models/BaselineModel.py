c79eb86033fdf1add3b1d981f045dcc20d1b01c9
cameroncruz <cameroncruz@gmail.com>
2020-05-23 23:12:11 -0700

    create: train_step for proper sequence decoding

--- src/models/BaselineModel.py
+++ src/models/BaselineModel.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
-from decoders.Decoder import Decoder
-from encoders.TemporalEncoder import TemporalEncoder
-from encoders.VideoEncoder import VideoEncoder
+from models.decoders.Decoder import Decoder
+from models.encoders.TemporalEncoder import TemporalEncoder
+from models.encoders.VideoEncoder import VideoEncoder
 
 
 class BaselineModel(tf.keras.Model):
     def __init__(
-        self, window_size, temporal_stride, embedding_dim, hidden_size, vocab_size
+        self, vocab_size, window_size=8, temporal_stride=4, embedding_dim=100, hidden_size=512
     ):
         super(BaselineModel, self).__init__()
 
@@ -16,15 +16,37 @@ class BaselineModel(tf.keras.Model):
             embedding_dim=embedding_dim,
         )
         self.temporal_encoder = TemporalEncoder(hidden_size=hidden_size)
-        self.decoder = Decoder(hidden_size=hidden_size, vocab_size=vocab_size)
+        self.decoder = Decoder(hidden_size=hidden_size, vocab_size=vocab_size, embedding_dim=embedding_dim)
 
-    def call(self, inputs, training=None, mask=None):
-        clip_embeddings = self.video_encoder(inputs)
-        encoder_output = self.temporal_encoder(clip_embeddings)
-        return self.decoder(encoder_output)
+    # def call(self, inputs, training=None, mask=None):
+    #     clip_embeddings = self.video_encoder(inputs)
+    #     encoder_output = self.temporal_encoder(clip_embeddings)
+    #     return self.decoder(encoder_output)
 
+    def train_step(self, data):
+        frame_features, targets = data
+        batch_size = targets.shape[0]
 
-if __name__ == "__main__":
-    model = BaselineModel(8, 4, 100, 1024, 1000)
-    model(tf.zeros((4, 16, 224, 224, 3)))
-    model.summary()
+        loss = 0
+
+        memory_state, carry_state = self.decoder.reset_state(batch_size=batch_size)
+        decoder_inputs = tf.expand_dims(targets[:, 0], 1)
+
+        with tf.GradientTape() as tape:
+            frame_encodings = self.video_encoder(frame_features)
+            clip_encodings = self.temporal_encoder(frame_encodings)
+
+            for i in range(1, targets.shape[1]):
+                preds, memory_state, carry_state = self.decoder([decoder_inputs, clip_encodings, memory_state, carry_state])
+
+                loss += self.compiled_loss(targets[:, i], preds)
+
+                decoder_inputs = tf.expand_dims(targets[:, i], 1)
+
+        trainable_variables = self.video_encoder.trainable_variables + self.temporal_encoder.trainable_variables + self.decoder.trainable_variables
+
+        gradients = tape.gradient(loss, trainable_variables)
+
+        self.optimizer.apply_gradients(zip(gradients, trainable_variables))
+
+        return {'loss': loss}
--- src/models/decoders/Decoder.py
+++ src/models/decoders/Decoder.py
@@ -2,18 +2,24 @@ import tensorflow as tf
 
 
 class Decoder(tf.keras.Model):
-    def __init__(self, hidden_size, vocab_size):
+    def __init__(self, hidden_size, vocab_size, embedding_dim):
         super(Decoder, self).__init__()
-        self.query = tf.keras.layers.Dense(units=512, activation=None)
-        self.value = tf.keras.layers.Dense(units=512, activation=None)
-        self.attention = tf.keras.layers.Attention(use_scale=True)
-        self.lstm = tf.keras.layers.LSTM(units=hidden_size, return_sequences=True)
-        self.classifier = tf.keras.layers.TimeDistributed(
-            tf.keras.layers.Dense(units=vocab_size, activation="softmax")
-        )
+        self.hidden_size = hidden_size
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.query = tf.keras.layers.Dense(units=hidden_size)
+        self.value = tf.keras.layers.Dense(units=hidden_size)
+        self.attention = tf.keras.layers.Attention(causal=True)
+        self.lstm = tf.keras.layers.LSTM(units=hidden_size, return_sequences=False, return_state=True)
+        self.classifier = tf.keras.layers.Dense(units=vocab_size, activation="softmax")
 
     def call(self, inputs, training=None, mask=None):
-        x = self.attention([self.query(inputs), self.value(inputs)])
-        x = self.lstm(x)
-        preds = self.classifier(x)
-        return preds
+        prev_word, features, memory_state, carry_state = inputs
+        x = self.embedding(prev_word)
+        context = self.attention([self.query(features), self.value(tf.expand_dims(memory_state, 1))])
+        x = tf.concat([tf.expand_dims(tf.reduce_sum(context, axis=1), 1), x], axis=-1)
+        output, memory_state, carry_state = self.lstm(x, initial_state=[memory_state, carry_state])
+        preds = self.classifier(output)
+        return preds, memory_state, carry_state
+
+    def reset_state(self, batch_size):
+        return tf.zeros((batch_size, self.hidden_size)), tf.zeros((batch_size, self.hidden_size))
